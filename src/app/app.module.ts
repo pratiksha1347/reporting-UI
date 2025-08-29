830bcb3fa26c38639d8413a666fd81bf52c2ecd9
Z8PKJ <pkj@kmd.dk>
2025-08-29 19:29:22 +0530

    Add footer and Header for the main view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppComponent } from './app.component';
 import {HeaderComponent} from "./modules/components/header/header.component";
 import {FooterComponent} from "./modules/components/footer/footer.component";
 import {HcmNgModule} from "hcm-angular";
+import { AgreementViewComponent } from './modules/components/agreement-view/agreement-view.component';
+import { ReportDownloadFormComponent } from './modules/components/report-download-form/report-download-form.component';
+import { AgreementCreateFormComponent } from './modules/components/agreement-create-form/agreement-create-form.component';
+import {RouterOutlet} from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AgreementViewComponent,
+    ReportDownloadFormComponent,
+    AgreementCreateFormComponent
   ],
   imports: [
     BrowserModule,
-    HcmNgModule
+    HcmNgModule,
+    RouterOutlet
   ],
   providers: [
     {
