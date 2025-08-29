2f7c7256f1d1dcbb8191ca2c000eb1a215e331ae
Z8GGO <ggo@kmd.dk>
2025-08-29 16:16:18 +0530

    Test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ReportFormComponent } from './modules/report-form/report-form.component';
-import { AgreementFormComponent } from './modules/agreement-form/agreement-form.component';
-import { AgreementViewComponent } from './modules/agreement-view/agreement-view.component';
-
 @NgModule({
   declarations: [
-    AppComponent,
-    ReportFormComponent,
-    AgreementFormComponent,
-    AgreementViewComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
