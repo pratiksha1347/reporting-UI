4fff5027e59240d5f0c87915421b427699e9255e
Z8PKJ <pkj@kmd.dk>
2025-08-29 20:39:15 +0530

    Minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from './app.component';
 import {HeaderComponent} from "./modules/components/header/header.component";
 import {FooterComponent} from "./modules/components/footer/footer.component";
 import {HcmNgModule} from "hcm-angular";
-import { AgreementViewComponent } from './modules/components/agreement-view/agreement-view.component';
 import { ReportDownloadFormComponent } from './modules/components/report-download-form/report-download-form.component';
 import { AgreementCreateFormComponent } from './modules/components/agreement-create-form/agreement-create-form.component';
 import {RouterOutlet} from "@angular/router";
@@ -16,7 +15,6 @@ import { AgreementViewTabComponent } from './modules/components/agreement-view-t
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    AgreementViewComponent,
     ReportDownloadFormComponent,
     AgreementCreateFormComponent,
     AgreementViewTabComponent
